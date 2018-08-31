fcb45bfeebb5bcedff7d4bd820efbf5cad29ac57
edwinfapp <edwinfapp@gmail.com>
2018-08-31 02:30:38 -0500

    adicion de nombre de rivales

--- Wezen/WebContent/app.js
+++ Wezen/WebContent/app.js
@@ -1,3 +1,17 @@
+var WINW = 0;
+var WINH = 0;
+
+function reziseWN(){
+	WINW = $(window).width();
+	WINH = $(window).height();
+}
+
+$(window).resize(function() {
+	reziseWN();
+});
+
+reziseWN();
+
 var CDATA = null;
 var app = angular.module('app', []);
 
@@ -11,9 +25,34 @@ app.controller('mainController', function mainController($scope, $interval) {
 	$interval(function() {
 
 		if (CDATA != null) {
+			
 			$scope.data = CDATA;
 			
 			$scope.cr = cleanArray(CDATA.cr);
+			
+			var cube = cars[IDCAR].cube;
+			var angu = cube.rotation.z;
+			
+			var x0 = cube.position.x;
+			var y0 = cube.position.y;
+			
+			$scope.cr.forEach(function(elem){
+				
+				var x1 = x0 + Math.cos(angu);
+				var y1 = y0 + Math.sin(angu);
+				
+				var x2 = elem.x;
+				var y2 = elem.y;
+				
+				elem.pt = null;
+				
+				if(x0 != x2 && y0 != y2){
+					
+					elem.pt = ajustarAngulo(angulo2P(x0, y0, x2,y2, x1,y1) + Math.PI/2);
+					elem.ang = parseInt((WINW/4) * WINW/WINH * elem.pt + WINW/2 -100);
+				}
+				
+			});
 			
 			var energia = Math.round(CDATA.cr[CDATA.me].e);
 			
@@ -43,6 +82,46 @@ app.controller('mainController', function mainController($scope, $interval) {
 });
 
 // ----
+
+function angulo(x,y){
+	
+	if(x==0){
+		return y>=0?Math.PI/2:-Math.PI/2;
+	}
+	
+	var a = Math.atan(y/x);
+	
+	if(x >= 0){
+		return ajustarAngulo(a);
+	}
+	
+	if( y > 0){
+		return ajustarAngulo(Math.PI + a);
+	}else{
+		return ajustarAngulo(Math.PI + a);
+	}
+	
+}
+
+function angulo2(x1,y1, x2,y2){
+	return ajustarAngulo(angulo(x2, y2) - angulo(x1, y1));
+}
+
+function angulo2P(x0, y0, x1,y1, x2,y2){
+	return angulo2(x1 - x0, y1 - y0, x2 - x0 , y2 - y0);
+}
+
+function ajustarAngulo(a){
+	while(a < -Math.PI){
+		a += 2*Math.PI;
+	}
+	
+	while(a > Math.PI){
+		a -= 2*Math.PI;
+	}
+	
+	return a;
+}
 
 function cleanArray(r) {
 	 var rs = [];
--- Wezen/WebContent/game.jsp
+++ Wezen/WebContent/game.jsp
@@ -44,10 +44,15 @@
 	
 		<div ng-repeat="reg in cr" class="registro_res" ng-class="{registro_sel: reg.me}">
 			<h2>{{ reg.n }}</h2>
-			<h3>{{ reg.p }}</h3> 
+			<h3 class="e_E" ng-class="{e_E3: reg.e > 75, e_E2: reg.e <= 75 && reg.e > 50, e_E1: reg.e <= 50 && reg.e > 25, e_E0: reg.e <= 25}">{{ reg.p }}</h3> 
 		</div>
 	
 	</div>
+	
+	<div ng-repeat="reg in cr" class="c_titulo" style="left: {{ reg.ang }}px" ng-if="reg.pt != null">
+		{{ reg.n }}<br>{{ reg.ang }}
+	</div>
+	
 	
 	<!-- - - - - - - - - - - - - - - - - - - - -->
 
